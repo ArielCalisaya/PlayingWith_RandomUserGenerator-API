16ccf4e2d219d7f3b36f4e6b534bfd4f9da90973
Ariel <calisayaariel62@gmail.com>
2019-08-12 04:37:54 -0400

    FetchAPI

--- src/App.js
+++ src/App.js
@@ -2,25 +2,42 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+
+class App extends React.Component {
+  constructor(props){
+    super(props);
+      // this.handleClick = this.handleClick.bind(this);
+      this.state= {
+        loading: true,
+        person: null
+      };
+  }
+  handleClick() {
+    let content= document.querySelector('#content')
+    console.log('The link was clicked.', this);
+}
+  async componentDidMount(){
+    const url = 'https://api.randomuser.me/';
+    const response = await fetch(url);
+    const data = await response.json();
+    this.setState({person: data.results[0], loading:false});
+    console.log(data.results)
+  }
+
+render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+        {this.state.loading || !this.state.person? <div>loading...</div> :
+      <div>
+        <h2>{this.state.person.name.title}</h2>
+        <h3>{this.state.person.name.first}</h3>
+        <h3>{this.state.person.name.last}</h3>
+        <img src={this.state.person.picture.large} />
+      </div>
+      }
 
+      </div>
+    );
+  }
+}
 export default App;
